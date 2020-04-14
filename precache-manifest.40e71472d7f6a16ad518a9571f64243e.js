2e8f24cc91f4f57f5c33457cbab0e7907166445a
Pisjun <nab1996@yandex.ru>
2020-04-14 23:17:52 +0300

    Updates

---
+++ precache-manifest.40e71472d7f6a16ad518a9571f64243e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9901541e2b57e72db88b",
+    "url": "/static/css/main.20592513.chunk.css"
+  },
+  {
+    "revision": "9901541e2b57e72db88b",
+    "url": "/static/js/main.e1b71b78.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f993cb28ebc1ac18ef27",
+    "url": "/static/css/2.fe764e58.chunk.css"
+  },
+  {
+    "revision": "f993cb28ebc1ac18ef27",
+    "url": "/static/js/2.3c150cf5.chunk.js"
+  },
+  {
+    "revision": "95ce0545afa08817abb81d1812e43a39",
+    "url": "/index.html"
+  }
+];
